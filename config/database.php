147886ce018fcad6d4e468288f6ea4f0fa41857c
captain-ashiki <ashikulislam2070@gmail.com>
2026-05-09 18:30:58 +0600

    sgsdhsd

--- config/database.php
+++ config/database.php
@@ -34,7 +34,7 @@ function getDB(): PDO
             PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
             PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
             PDO::ATTR_EMULATE_PREPARES => false,
-            PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8mb4 COLLATE utf8mb4_unicode_ci"
+            PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8mb4 COLLATE utf8mb4_unicode_ci; SET time_zone = '+06:00';"
         ];
 
         try {
